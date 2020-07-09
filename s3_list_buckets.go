439121ade66308752c73f2fe451f5c1fdee9cd0b
Peter Eneroth <peteren@axis.com>
2020-07-09 14:39:39 +0200

    Comments cleanup

--- s3_list_buckets.go
+++ s3_list_buckets.go
@@ -1,24 +1,7 @@
-// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-sourceauthor:[Doug-AWS]
-// snippet-sourcedescription:[Lists your S3 buckets.]
-// snippet-keyword:[Amazon Simple Storage Service]
-// snippet-keyword:[Amazon S3]
-// snippet-keyword:[ListBuckets function]
-// snippet-keyword:[Go]
-// snippet-sourcesyntax:[go]
-// snippet-service:[s3]
-// snippet-keyword:[Code Sample]
-// snippet-sourcetype:[full-example]
-// snippet-sourcedate:[2018-03-16]
+
 /*
-   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
-    http://aws.amazon.com/apache2.0/
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
+   This is a test program based on an example file from Amazon
+   The program lists files in a S3 bucket
 */
 
 package main
